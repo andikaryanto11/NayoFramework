32347c4575c884f074503509f3d360b1447e092d
andikaryanto11 <42728084+andikaryanto11@users.noreply.github.com>
2020-02-14 16:43:11 +0700

    Update Routes.php

--- App/Config/Routes.php
+++ App/Config/Routes.php
@@ -1,8 +1,5 @@
 <?php
 
-define('DS', DIRECTORY_SEPARATOR, true);
-define('BASE_PATH', __DIR__ . DS, TRUE);
-
 $app		= System\App::instance();
 $app->request  	= System\Request::instance();
 $app->route	= System\Route::instance($app->request);
